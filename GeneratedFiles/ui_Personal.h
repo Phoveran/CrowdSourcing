0a1fe7de155ea201d8c8e804e3b33bce2145002e
Phoveran <dauntlessstan@foxmail.com>
2019-07-11 20:33:40 +0800

    update

--- GeneratedFiles/ui_Personal.h
+++ GeneratedFiles/ui_Personal.h
@@ -10,9 +10,13 @@
 #define UI_PERSONAL_H
 
 #include <QtCore/QVariant>
+#include <QtGui/QIcon>
+#include <QtWidgets/QAction>
 #include <QtWidgets/QApplication>
 #include <QtWidgets/QMainWindow>
+#include <QtWidgets/QMenu>
 #include <QtWidgets/QMenuBar>
+#include <QtWidgets/QTextEdit>
 #include <QtWidgets/QWidget>
 
 QT_BEGIN_NAMESPACE
@@ -21,7 +25,11 @@ class Ui_MainWindow
 {
 public:
     QWidget *centralwidget;
+    QTextEdit *textEdit;
     QMenuBar *menubar;
+    QMenu *menu;
+    QMenu *menu_2;
+    QMenu *menu_3;
 
     void setupUi(QMainWindow *MainWindow)
     {
@@ -30,11 +38,34 @@ public:
         MainWindow->resize(1920, 1106);
         centralwidget = new QWidget(MainWindow);
         centralwidget->setObjectName(QString::fromUtf8("centralwidget"));
+        textEdit = new QTextEdit(centralwidget);
+        textEdit->setObjectName(QString::fromUtf8("textEdit"));
+        textEdit->setGeometry(QRect(180, 100, 241, 51));
         MainWindow->setCentralWidget(centralwidget);
         menubar = new QMenuBar(MainWindow);
         menubar->setObjectName(QString::fromUtf8("menubar"));
         menubar->setGeometry(QRect(0, 0, 1920, 26));
+        menu = new QMenu(menubar);
+        menu->setObjectName(QString::fromUtf8("menu"));
+        menu->setStyleSheet(QString::fromUtf8(""));
+        QIcon icon;
+        icon.addFile(QString::fromUtf8(":/CrowdSourcing/Resources/pictures/\351\246\226\351\241\265.png"), QSize(), QIcon::Normal, QIcon::Off);
+        menu->setIcon(icon);
+        menu_2 = new QMenu(menubar);
+        menu_2->setObjectName(QString::fromUtf8("menu_2"));
+        QIcon icon1;
+        icon1.addFile(QString::fromUtf8(":/CrowdSourcing/Resources/pictures/\344\270\252\344\272\272\344\270\255\345\277\203_\346\210\221\347\232\204\344\277\241\346\201\257.png"), QSize(), QIcon::Normal, QIcon::Off);
+        menu_2->setIcon(icon1);
+        menu_3 = new QMenu(menubar);
+        menu_3->setObjectName(QString::fromUtf8("menu_3"));
+        QIcon icon2;
+        icon2.addFile(QString::fromUtf8(":/CrowdSourcing/Resources/pictures/\344\273\273\345\212\241.png"), QSize(), QIcon::Normal, QIcon::Off);
+        menu_3->setIcon(icon2);
         MainWindow->setMenuBar(menubar);
+
+        menubar->addAction(menu->menuAction());
+        menubar->addAction(menu_2->menuAction());
+        menubar->addAction(menu_3->menuAction());
 
         retranslateUi(MainWindow);
 
@@ -43,7 +74,10 @@ public:
 
     void retranslateUi(QMainWindow *MainWindow)
     {
-        MainWindow->setWindowTitle(QCoreApplication::translate("MainWindow", "MainWindow", nullptr));
+        MainWindow->setWindowTitle(QCoreApplication::translate("MainWindow", "CrowdSourcing", nullptr));
+        menu->setTitle(QCoreApplication::translate("MainWindow", "\351\246\226\351\241\265", nullptr));
+        menu_2->setTitle(QCoreApplication::translate("MainWindow", "\344\270\252\344\272\272\344\277\241\346\201\257", nullptr));
+        menu_3->setTitle(QCoreApplication::translate("MainWindow", "\344\273\273\345\212\241", nullptr));
     } // retranslateUi
 
 };
