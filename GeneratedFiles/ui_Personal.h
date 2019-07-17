831706555f3bab0e042792c801ca5448696b8dda
Phoveran <dauntlessstan@foxmail.com>
2019-07-17 16:45:17 +0800

    update

--- GeneratedFiles/ui_Personal.h
+++ GeneratedFiles/ui_Personal.h
@@ -13,9 +13,13 @@
 #include <QtGui/QIcon>
 #include <QtWidgets/QAction>
 #include <QtWidgets/QApplication>
+#include <QtWidgets/QLabel>
+#include <QtWidgets/QLineEdit>
 #include <QtWidgets/QMainWindow>
 #include <QtWidgets/QMenu>
 #include <QtWidgets/QMenuBar>
+#include <QtWidgets/QPushButton>
+#include <QtWidgets/QVBoxLayout>
 #include <QtWidgets/QWidget>
 
 QT_BEGIN_NAMESPACE
@@ -24,6 +28,21 @@ class Ui_MainWindow
 {
 public:
     QWidget *centralwidget;
+    QWidget *verticalLayoutWidget;
+    QVBoxLayout *verticalLayout;
+    QLabel *label;
+    QLabel *label_2;
+    QLabel *label_3;
+    QLabel *label_4;
+    QLabel *label_5;
+    QWidget *verticalLayoutWidget_2;
+    QVBoxLayout *verticalLayout_2;
+    QLineEdit *lineEdit;
+    QLineEdit *lineEdit_3;
+    QLineEdit *lineEdit_5;
+    QLineEdit *lineEdit_4;
+    QLineEdit *lineEdit_2;
+    QPushButton *pushButton;
     QMenuBar *menubar;
     QMenu *menu;
     QMenu *menu_2;
@@ -36,6 +55,103 @@ public:
         MainWindow->resize(1920, 1106);
         centralwidget = new QWidget(MainWindow);
         centralwidget->setObjectName(QString::fromUtf8("centralwidget"));
+        verticalLayoutWidget = new QWidget(centralwidget);
+        verticalLayoutWidget->setObjectName(QString::fromUtf8("verticalLayoutWidget"));
+        verticalLayoutWidget->setGeometry(QRect(50, 50, 228, 451));
+        verticalLayout = new QVBoxLayout(verticalLayoutWidget);
+        verticalLayout->setObjectName(QString::fromUtf8("verticalLayout"));
+        verticalLayout->setContentsMargins(0, 0, 0, 0);
+        label = new QLabel(verticalLayoutWidget);
+        label->setObjectName(QString::fromUtf8("label"));
+        QFont font;
+        font.setFamily(QString::fromUtf8("Century Gothic"));
+        font.setPointSize(20);
+        font.setBold(true);
+        font.setItalic(true);
+        font.setWeight(75);
+        label->setFont(font);
+
+        verticalLayout->addWidget(label);
+
+        label_2 = new QLabel(verticalLayoutWidget);
+        label_2->setObjectName(QString::fromUtf8("label_2"));
+        label_2->setFont(font);
+
+        verticalLayout->addWidget(label_2);
+
+        label_3 = new QLabel(verticalLayoutWidget);
+        label_3->setObjectName(QString::fromUtf8("label_3"));
+        label_3->setFont(font);
+
+        verticalLayout->addWidget(label_3);
+
+        label_4 = new QLabel(verticalLayoutWidget);
+        label_4->setObjectName(QString::fromUtf8("label_4"));
+        label_4->setFont(font);
+
+        verticalLayout->addWidget(label_4);
+
+        label_5 = new QLabel(verticalLayoutWidget);
+        label_5->setObjectName(QString::fromUtf8("label_5"));
+        label_5->setFont(font);
+
+        verticalLayout->addWidget(label_5);
+
+        verticalLayoutWidget_2 = new QWidget(centralwidget);
+        verticalLayoutWidget_2->setObjectName(QString::fromUtf8("verticalLayoutWidget_2"));
+        verticalLayoutWidget_2->setGeometry(QRect(280, 50, 411, 451));
+        verticalLayout_2 = new QVBoxLayout(verticalLayoutWidget_2);
+        verticalLayout_2->setSpacing(30);
+        verticalLayout_2->setObjectName(QString::fromUtf8("verticalLayout_2"));
+        verticalLayout_2->setContentsMargins(0, 0, 0, 0);
+        lineEdit = new QLineEdit(verticalLayoutWidget_2);
+        lineEdit->setObjectName(QString::fromUtf8("lineEdit"));
+        QFont font1;
+        font1.setFamily(QString::fromUtf8("Courier New"));
+        font1.setPointSize(20);
+        lineEdit->setFont(font1);
+        lineEdit->setReadOnly(true);
+
+        verticalLayout_2->addWidget(lineEdit);
+
+        lineEdit_3 = new QLineEdit(verticalLayoutWidget_2);
+        lineEdit_3->setObjectName(QString::fromUtf8("lineEdit_3"));
+        lineEdit_3->setFont(font1);
+        lineEdit_3->setReadOnly(true);
+
+        verticalLayout_2->addWidget(lineEdit_3);
+
+        lineEdit_5 = new QLineEdit(verticalLayoutWidget_2);
+        lineEdit_5->setObjectName(QString::fromUtf8("lineEdit_5"));
+        lineEdit_5->setFont(font1);
+        lineEdit_5->setReadOnly(true);
+
+        verticalLayout_2->addWidget(lineEdit_5);
+
+        lineEdit_4 = new QLineEdit(verticalLayoutWidget_2);
+        lineEdit_4->setObjectName(QString::fromUtf8("lineEdit_4"));
+        lineEdit_4->setFont(font1);
+        lineEdit_4->setReadOnly(true);
+
+        verticalLayout_2->addWidget(lineEdit_4);
+
+        lineEdit_2 = new QLineEdit(verticalLayoutWidget_2);
+        lineEdit_2->setObjectName(QString::fromUtf8("lineEdit_2"));
+        lineEdit_2->setFont(font1);
+        lineEdit_2->setReadOnly(true);
+
+        verticalLayout_2->addWidget(lineEdit_2);
+
+        pushButton = new QPushButton(centralwidget);
+        pushButton->setObjectName(QString::fromUtf8("pushButton"));
+        pushButton->setGeometry(QRect(200, 530, 301, 41));
+        QFont font2;
+        font2.setFamily(QString::fromUtf8("Century Gothic"));
+        font2.setPointSize(14);
+        font2.setBold(true);
+        font2.setItalic(true);
+        font2.setWeight(75);
+        pushButton->setFont(font2);
         MainWindow->setCentralWidget(centralwidget);
         menubar = new QMenuBar(MainWindow);
         menubar->setObjectName(QString::fromUtf8("menubar"));
@@ -70,6 +186,14 @@ public:
     void retranslateUi(QMainWindow *MainWindow)
     {
         MainWindow->setWindowTitle(QCoreApplication::translate("MainWindow", "CrowdSourcing", nullptr));
+        label->setText(QCoreApplication::translate("MainWindow", "Username:", nullptr));
+        label_2->setText(QCoreApplication::translate("MainWindow", "Member type:", nullptr));
+        label_3->setText(QCoreApplication::translate("MainWindow", "Credits:", nullptr));
+        label_4->setText(QCoreApplication::translate("MainWindow", "Balance:", nullptr));
+        label_5->setText(QCoreApplication::translate("MainWindow", "Certification:", nullptr));
+        lineEdit->setText(QString());
+        lineEdit_5->setInputMask(QString());
+        pushButton->setText(QCoreApplication::translate("MainWindow", "Update my information", nullptr));
         menu->setTitle(QCoreApplication::translate("MainWindow", "\351\246\226\351\241\265", nullptr));
         menu_2->setTitle(QCoreApplication::translate("MainWindow", "\344\270\252\344\272\272\344\277\241\346\201\257", nullptr));
         menu_3->setTitle(QCoreApplication::translate("MainWindow", "\344\273\273\345\212\241", nullptr));
